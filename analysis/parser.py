307d273fe225fe7f81e976d3cd5503eef56bd4fa
Ubuntu <ubuntu@ip-172-31-84-85.ec2.internal>
2019-04-25 19:59:59 +0000

    analysis: Add full req RTT latency, use cli args for script

--- analysis/parser.py
+++ analysis/parser.py
@@ -3,11 +3,18 @@ from matplotlib import cm
 import numpy as np
 import json
 import sys
+import os
+import argparse
 
 #aws_latency
 
-def aws_latency(response):
-	return response['json']['triggeredTime'] - response['timings']['upload']
+def calc_latency(response, method="upload_rtt_adj"):
+    if method == "upload_rtt_adj":
+        tcp_latency = response['timings']['phases']['tcp'] / 2
+        client_trigger_time = response['timings']['upload'] - tcp_latency
+        return response['json']['triggeredTime'] - client_trigger_time
+    elif method == "req_rtt":
+        return response['timings']['response'] - response['timings']['upload']
 # round: this._tick,
 # window
 # size
@@ -18,160 +25,161 @@ def aws_latency(response):
 # x_axis: "tick"
 # y_axis: "latency", "cold_v_warm"
 def over_time(responses, y_axis):
-	window = responses[0].window
+    window = responses[0].window
 
-	if y_axis == "latency":
-		y_tmp = {}
-		last_round = 0
-		for response in responses:
-			last_round = max(last_round, response['round'])
-			y_tmp.setdefault(response['round'], []).append(aws_latency(response))
-		stdev = [0 for i in range(last_round+1)]
-		y_avg = [0 for i in range(last_round+1)]
-		x_time = [0 for i in range(last_round+1)]
-		for i in range(last_round+1):
-			stdev[i] = np.std(y_tmp[i])
-			y_avg[i] = np.average(y_tmp[i])
-			x_time[i] = (i+1)*window
+    if y_axis == "latency":
+        y_tmp = {}
+        last_round = 0
+        for response in responses:
+            last_round = max(last_round, response['round'])
+            y_tmp.setdefault(response['round'], []).append(calc_latency(response))
+        stdev = [0 for i in range(last_round+1)]
+        y_avg = [0 for i in range(last_round+1)]
+        x_time = [0 for i in range(last_round+1)]
+        for i in range(last_round+1):
+            stdev[i] = np.std(y_tmp[i])
+            y_avg[i] = np.average(y_tmp[i])
+            x_time[i] = (i+1)*window
 
-		plt.errorbar(x_time, y_avg, yerr=stdev, fmt='go')
-		plt.xlabel('Time (ms)', fontsize=18)
-		plt.ylabel('Latency (ms)', fontsize=16)
+        plt.errorbar(x_time, y_avg, yerr=stdev, fmt='go')
+        plt.xlabel('Time (ms)', fontsize=18)
+        plt.ylabel('Latency (ms)', fontsize=16)
 
-		fig = plt.figure()
-		fig.savefig(filename + "_latency_time" + '.png')
+        fig = plt.figure()
+        fig.savefig(out_prefix + "_latency_time" + '.png')
 
-		results = zip(x_time, y_avg, stdev)
-		return results
+        results = zip(x_time, y_avg, stdev)
+        return results
 
-	if y_axis == "cold_v_warm":
-		y_new = [0 for i in range(last_round+1)]
-		y_old = [0 for i in range(last_round+1)]
-		y_total = [0 for i in range(last_round+1)]
-		last_round = 0
+    if y_axis == "cold_v_warm":
+        y_new = [0 for i in range(last_round+1)]
+        y_old = [0 for i in range(last_round+1)]
+        y_total = [0 for i in range(last_round+1)]
+        last_round = 0
 
-		for response in responses:
-			last_round = max(last_round, response['round'])
-			y_total[response['round']] += 1
-			if response["runCount"] > 0:
-				y_old[response['round']] += 1
-			else:
-				y_new[response['round']] += 1
+        for response in responses:
+            last_round = max(last_round, response['round'])
+            y_total[response['round']] += 1
+            if response["runCount"] > 0:
+                y_old[response['round']] += 1
+            else:
+                y_new[response['round']] += 1
 
-		x_time = [0 for i in range(last_round+1)]
-		for i in range(last_round+1):
-			x_time[i] = (i+1)*window
+        x_time = [0 for i in range(last_round+1)]
+        for i in range(last_round+1):
+            x_time[i] = (i+1)*window
 
-		plt.plot(x_time, y_total, 'k.', x_time, y_new, 'co', x_time, y_old, 'g^')
-		plt.xlabel('Time (ms)', fontsize=18)
-		plt.ylabel('# of VMs', fontsize=16)
+        plt.plot(x_time, y_total, 'k.', x_time, y_new, 'co', x_time, y_old, 'g^')
+        plt.xlabel('Time (ms)', fontsize=18)
+        plt.ylabel('# of VMs', fontsize=16)
 
-		fig = plt.figure()
-		fig.savefig(filename + "_cold_v_warm" + '.png')
+        fig = plt.figure()
+        fig.savefig(out_prefix + "_cold_v_warm" + '.png')
 
-		results = zip(x_time, y_total, y_old, y_new)
-		return results
-	return "valid options: latency or cold_v_warm"
+        results = zip(x_time, y_total, y_old, y_new)
+        return results
+    return "valid options: latency or cold_v_warm"
 
 # x_axis: latency
 # y_axis: percent
-def cdf(responses, options=None):
-	responses = sorted(responses, key=lambda response: aws_latency(response))
+def cdf(responses, out_prefix, filter="all", latency="upload_rtt_adj"):
+    filtered_resp = sorted(responses, key=lambda response: calc_latency(response, method=latency))
 
-	if options=="cold":
-		cold = []
-		for response in responses:
-			if response['json']['runCount'] == 0:
-				cold.append(response)
-		responses = cold
+    if filter=="cold":
+        resp = []
+        for response in filtered_resp:
+            if response['json']['runCount'] == 1:
+                resp.append(response)
+        filtered_resp = resp
 
-	if options=="warm":
-		print("HI")
-		warm = []
-		for response in responses:
-			if response['json']['runCount'] > 0:
-				warm.append(response)
-		responses = warm
+    if filter=="warm":
+        resp = []
+        for response in filtered_resp:
+            if response['json']['runCount'] > 1:
+                resp.append(response)
+        filtered_resp = resp
 
-	points = len(responses)
-	y = [0]
-	x = [0]
-	for i in range(0, points):
-		x.append(aws_latency(responses[i]))
-		y.append(float(i) / points * 100)
+    num_points = len(filtered_resp)
+    x = [0]
+    y = [0]
+    for i in range(0, num_points):
+        x.append(calc_latency(filtered_resp[i], method=latency))
+        y.append(float(i) / num_points * 100)
 
-	plt.plot(x, y, '-')
-	plt.xlabel('Latency (ms)', fontsize=18)
-	plt.ylabel('% of VMs', fontsize=16)
-	plt.show()
-	plt.savefig(filename + "_latency_CDF_" + options + '.png')
+    plt.plot(x, y, '-')
+    plt.xlabel('Latency (ms, {})'.format(latency), fontsize=18)
+    plt.ylabel('% of VMs (n={})'.format(num_points), fontsize=16)
+    plt.show()
+    plt.savefig(out_prefix + "_latency_CDF_" + latency + "_" + filter + '.png')
 
-	print(zip(x, y))
-	return zip(x, y)
+    print(zip(x, y))
+    return zip(x, y)
 
 def cdf_helper(responses, max_latency, points):
-	total = len(responses)
-	responses = sorted(responses, key=lambda response: aws_latency(response))
-	x = [i*max_latency/(points-1) for i in range(points)]
-	y = []
-	cur = 0
-	for latency in x:
-		while responses and aws_latency(responses[0]) <= latency:
-			cur += 1
-			responses.pop()
-		y.append(cur/total)
-	return y
+    total = len(responses)
+    responses = sorted(responses, key=lambda response: calc_latency(response))
+    x = [i*max_latency/(points-1) for i in range(points)]
+    y = []
+    cur = 0
+    for latency in x:
+        while responses and calc_latency(responses[0]) <= latency:
+            cur += 1
+            responses.pop()
+        y.append(cur/total)
+    return y
 
 
 # y_axis: latency
 # x_axis: round
 # z_axis: percent
 # yeah this part is gonna be weird
-def cdf_3d(responses):
-	max_latency = 0
-	last_round = 0
-	rounds = {}
-	for response in responses:
-		rounds.setdefault(response['round'], []).append(response)
-		max_latency = max(max_latency, aws_latency(response))
-		last_round = max(last_round, response['round'])
+def cdf_3d(responses, out_prefix):
+    max_latency = 0
+    last_round = 0
+    rounds = {}
+    for response in responses:
+        rounds.setdefault(response['round'], []).append(response)
+        max_latency = max(max_latency, calc_latency(response))
+        last_round = max(last_round, response['round'])
 
-	x = np.linspace(0, last_round, last_round+1)
-	y = np.linspace(0, max_latency, 101)
+    x = np.linspace(0, last_round, last_round+1)
+    y = np.linspace(0, max_latency, 101)
 
-	xx, yy = np.meshgrid(x, y)
+    xx, yy = np.meshgrid(x, y)
 
 
-	z = []
-	for i in range(last_round+1):
-		z.append(cdf_helper(rounds[i], max_latency, 101))
-	np.transpose(z)
-	fig = plt.figure()
-	ax = fig.gca(projection='3d')
-	surf = ax.plot_surface(xx, yy, z, cmap=cm.coolwarm,
+    z = []
+    for i in range(last_round+1):
+        z.append(cdf_helper(rounds[i], max_latency, 101))
+    np.transpose(z)
+    fig = plt.figure()
+    ax = fig.gca(projection='3d')
+    surf = ax.plot_surface(xx, yy, z, cmap=cm.coolwarm,
                        linewidth=0, antialiased=False)
-	fig.savefig(filename + "_cdf_over_time" + '.jpg')
-
-filename = None
-
-def main():
-	global filename
-	filename = sys.argv[1]
-	file = open(filename)
-	f_data = json.load(file)
-	responses = f_data['responses']
-	for r in responses:
-		r['json'] = json.loads(r['body'])
-
-	graph = sys.argv[2]
-	options = sys.argv[3] if len(sys.argv) > 3 else None
-	if graph == "3d_cdf":
-		return cdf_3d(responses)
-	if graph == "cdf":
-		return cdf(responses, options)
-	if graph == "line":
-		# options: "latency", "cold_v_warm"
-		return over_time(responses, options)
+    fig.savefig(out_prefix + "_cdf_over_time" + '.jpg')
 
 if __name__== "__main__":
-	main()
+    parser = argparse.ArgumentParser()
+    parser.add_argument("data_file", help="file containing response data to load")
+    parser.add_argument("graph_type", help="which type of graph to generate",
+                        choices=["cdf", "3d_cdf", "line"])
+    args, extra_args_raw = parser.parse_known_args()
+    with open(args.data_file, 'r') as data_file:
+        data = json.load(data_file)
+        if data['errors']:
+            print('{} error responses'.format(len(data['errors'])))
+        responses = data['responses']
+        for r in responses:
+            r['json'] = json.loads(r['body'])
+
+    extra_args = {s.split("=")[0].replace("--", ""): s.split("=")[1] for s in extra_args_raw}
+
+    out_prefix = os.path.splitext(os.path.basename(args.data_file))[0]
+
+    if args.graph_type == "3d_cdf":
+        cdf_3d(responses, out_prefix)
+    if args.graph_type == "cdf":
+        cdf(responses, out_prefix, **extra_args)
+    if args.graph_type == "line":
+        # options: "latency", "cold_v_warm"
+        over_time(responses, out_prefix, **extra_args)
