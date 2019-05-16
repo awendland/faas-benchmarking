749cef94f844733658b4dee206c5e49003eb123b
Ubuntu <ubuntu@ip-172-31-84-85.ec2.internal>
2019-05-16 05:38:16 +0000

    faas-handle-cost: Ensure the handler is being awaited on (since it's async)

--- experiments/faas-handle-cost/index.js
+++ experiments/faas-handle-cost/index.js
@@ -2,20 +2,27 @@ const log = console.log
 console.log = () => {}
 const faasSrc = require('../../apps/faas/index')
 
-const numRuns = 1e6
-const startTime = Date.now()
-for (let i = 0; i < numRuns; ++i) {
-  faasSrc.handler(
-    {},
-    {
-      functionName: '',
-      functionVersion: '',
-      invokedFunctionArn: '',
-      awsRequestId: '',
-    },
+const run = async () => {
+  const numRuns = 1e6
+  const startTime = Date.now()
+  for (let i = 0; i < numRuns; ++i) {
+    await faasSrc.handler(
+      {},
+      {
+        functionName: '',
+        functionVersion: '',
+        invokedFunctionArn: '',
+        awsRequestId: '',
+      },
+    )
+  }
+  const elapsedTime = Date.now() - startTime
+  log(
+    `Took ${elapsedTime}ms for ${numRuns} runs = ${elapsedTime / numRuns}ms/run`,
   )
 }
-const elapsedTime = Date.now() - startTime
-log(
-  `Took ${elapsedTime}ms for ${numRuns} runs = ${elapsedTime / numRuns}ms/run`,
-)
+
+run().catch(e => {
+  console.error(e)
+  process.exit(1)
+})
