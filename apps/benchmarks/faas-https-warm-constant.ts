126155dce42793e75ca96ad6aeee58aa25931372
Ubuntu <ubuntu@ip-172-31-84-85.ec2.internal>
2019-05-19 03:23:22 +0000

    https: Remove function sleep since autocannon is a closed test

--- apps/benchmarks/faas-https-warm-constant.ts
+++ apps/benchmarks/faas-https-warm-constant.ts
@@ -6,13 +6,12 @@ import { createHttpsBenchmark } from './lib/faas-https-shared'
 
 createHttpsBenchmark({
   benchmarkName: 'warm-constant',
-  // Cloudformation max is 200 resources which is ~10 https fns
   numberOfFunctions: 1,
   initialMsgPerSec: 50,
   incrementMsgPerSec: 0,
   incrementPeriod: 10 * 1e3,
   numberOfPeriods: 20,
-  functionSleep: 5000,
+  functionSleep: undefined,
 }).catch(e => {
   console.error(e)
   process.exit(1)
--- apps/benchmarks/faas-https-warm-scaling.ts
+++ apps/benchmarks/faas-https-warm-scaling.ts
@@ -6,13 +6,12 @@ import { createHttpsBenchmark } from './lib/faas-https-shared'
 
 createHttpsBenchmark({
   benchmarkName: 'warm-scaling',
-  // Cloudformation max is 200 resources which is ~10 https fns
   numberOfFunctions: 1,
   initialMsgPerSec: 5,
   incrementMsgPerSec: 5,
   incrementPeriod: 10 * 1e3,
   numberOfPeriods: 20,
-  functionSleep: 5000,
+  functionSleep: undefined,
 }).catch(e => {
   console.error(e)
   process.exit(1)
